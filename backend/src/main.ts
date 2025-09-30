0dbee38d025e8d40d677b2b37a07831a7d5d0f66
santosrivaldo <77171407+santosrivaldo@users.noreply.github.com>
2025-09-30 12:26:30 -0300

    Refactor compression import in main.ts to use namespace import syntax

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import compression from 'compression';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
